dca6bd9bef592274b36bf58bb58b74fea92b5e59
yanepumba <adress>
2023-11-16 20:11:08 +0300

    added Fist_DZ

---
+++ Fist_DZ/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void PrintNumbers(int n)
+{
+    if (n <= 1)
+    {
+        Console.Write($"{n}\t");
+        return;
+    }
+    Console.Write($"{n}\t");
+    PrintNumbers(n - 1);
+}
+
+int n = ReadInt("Введите натуральное число: ");
+while (n < 1)
+{
+    Console.Write("Число не натуральное. ");
+    n = ReadInt("Введите натуральное число: ");
+}
+PrintNumbers(n);
